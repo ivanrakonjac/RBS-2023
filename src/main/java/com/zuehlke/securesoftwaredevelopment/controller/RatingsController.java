50cad468f6edd8fdad48bd2ec2757d4062b6d076
ivanrakonjac <rakonjacivan98@gmail.com>
2023-02-07 16:09:28 +0100

    RATE_MOVIE permission check added

--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/RatingsController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/RatingsController.java
@@ -1,13 +1,11 @@
 package com.zuehlke.securesoftwaredevelopment.controller;
 
-import com.zuehlke.securesoftwaredevelopment.domain.Comment;
 import com.zuehlke.securesoftwaredevelopment.domain.Rating;
 import com.zuehlke.securesoftwaredevelopment.domain.User;
-import com.zuehlke.securesoftwaredevelopment.repository.CommentRepository;
 import com.zuehlke.securesoftwaredevelopment.repository.RatingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +22,7 @@ public class RatingsController {
     }
 
     @PostMapping(value = "/ratings", consumes = "application/json")
+    @PreAuthorize("hasAuthority('RATE_MOVIE')")
     public String createOrUpdateRating(@RequestBody Rating rating, Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         rating.setUserId(user.getId());
